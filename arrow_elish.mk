e97e9287eed7d702554e0509bc1f9935335c7140
kubersharma001 <kubersharma001@gmail.com>
2022-10-18 08:50:30 +0000

    elish: Setup flags for ArrowOS Infra

--- arrow_elish.mk
+++ arrow_elish.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/elish/device.mk)
 
-# Inherit common LineageOS configurations
+# Inherit common configurations
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
@@ -19,9 +19,15 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := elish
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := M2105K81AC
+PRODUCT_MODEL := Xiaomi Pad 5 Pro
 PRODUCT_NAME := arrow_elish
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/elish/elish:13/RKQ1.211001.001/V14.0.3.0.TKYCNXM:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=M2105K81AC
+
+# ArrowOS additions
+DEVICE_MAINTAINER := EdwardWu
